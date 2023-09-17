61af5bcc3a75a074577e29237f71020fffd3e217
bri <bcamesi@gmail.com>
2023-09-17 19:41:11 -0400

    student view pages HTML  and routes

---
+++ client/src/pages/student/GoalsNeeds.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const GoalsNeeds = () => {
+  return (
+    <>
+    <div>
+    <h2>What’s your most important goal for the day?</h2>
+
+    <div>
+    <div><button>Finish homework during study hall</button></div>
+    <div><button>Better manage my energy</button></div>
+
+    <div><button>Do my best in class</button></div>
+    <div><button>Be more present</button></div>
+
+    <button>Something else</button>
+    </div>
+
+    <h2>What do you need from an adult to succeed today?</h2>
+
+    <div>
+    <div><button>Check in with my teacher</button></div>
+    <div><button>Help with homework</button></div>
+
+    <div><button>Extra practice</button></div>
+    <div><button>Help with focusing</button></div>
+
+    <button>Something else</button>
+    </div>
+
+    </div>
+    </>
+  );
+}
+
+export default GoalsNeeds;

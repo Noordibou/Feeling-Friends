a1cb21d63d04d29c854951f9966e52b32611094b
noordibou <noor.dibou@hotmail.com>
2023-09-21 22:20:32 -0500

    updating server index.js

--- server/index.js
+++ server/index.js
@@ -12,16 +12,8 @@ app.use(express.json());
 require('dotenv').config();
 require('./config/database');
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://mindful-journal.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', true);
-  next();
-});
-
 app.use(cors({
-  origin: 'https://mindful-journal.vercel.app/',
+  origin: 'https://mindful-journal.vercel.app',
   methods: 'GET, POST, PUT, DELETE',
   credentials: true,
 }));

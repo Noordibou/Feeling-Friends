20b8e2f146c6ddbdd9e648fe03a5ef5136da1610
noordibou <noor.dibou@hotmail.com>
2023-09-21 22:15:12 -0500

    fixing error

--- server/index.js
+++ server/index.js
@@ -20,11 +20,11 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use(cors({
-//   origin: 'https://mindful-journal.vercel.app/',
-//   methods: 'GET, POST, PUT, DELETE',
-//   credentials: true,
-// }));
+app.use(cors({
+  origin: 'https://mindful-journal.vercel.app/',
+  methods: 'GET, POST, PUT, DELETE',
+  credentials: true,
+}));
 
 
 app.use('/', require("./routes/authRoute"));

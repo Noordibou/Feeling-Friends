2b4722ebb51418f62818b7fbc7a4f160150e5037
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-10-11 18:30:34 -0500

    now able to get x-axis coordinates of the circle slider

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -4,24 +4,50 @@ import { motion } from 'framer-motion'
 const Slider = () => {
     const [value, setValue] = useState(0)
     let constraintsRef = useRef();
+    const handleRef= useRef();
+    const progressBarRef = useRef();
+    const handleSize = 30;
 
     const handleDrag = () => {
-
+        let max = 100;
+        let min = 0;
+        let handleBounds = handleRef.current.getBoundingClientRect();
+        let middleOfHandle = handleBounds.x + handleBounds.width / 2;
+        let progressBarBounds  = progressBarRef.current.getBoundingClientRect();
+        let newProgress = (middleOfHandle - progressBarBounds.x) / progressBarBounds.width;
+        // console.log("Position x-axis: " + Math.round(newProgress * 100))
     }
 
     return (
         <>
             <div className="">
                 <div className="relative flex pt-28 flex-col items-center justify-center">
-                    <div className="absolute w-3/4 h-2 bg-lightGray rounded-full" />
+                    
+                    {/* slider bar */}
+                    <div 
+                        className="absolute w-3/4 h-3 bg-lightGray rounded-full" 
+                        ref ={progressBarRef}    
+                    />
+
+                    {/* for slider progress */}
+                    <div 
+                        className="absolute flex w-3/4 h-1 bg-darkTeal items-center" 
+                    />
+
+                    {/* slider star */}
                     <div className="w-3/4" ref={constraintsRef}>
                         <motion.div 
-                            className="relative w-6 h-6 bg-green rounded-full" 
+                            className="relative w-10 h-10 bg-green rounded-full bg-opacity-70" 
+                            ref={handleRef}
                             drag="x" 
                             dragMomentum={false} 
                             dragConstraints={constraintsRef}
                             dragElastic={0}
                             onDrag={handleDrag}
+                            style={{
+                                width: handleSize,
+                                height: handleSize
+                            }}
                         />
                     </div>
                     

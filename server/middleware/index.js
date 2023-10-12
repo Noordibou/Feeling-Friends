bf0b8ced0d9fbc3ce5b1be6b128a65a296b16a3e
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-10-12 13:17:45 -0500

    planning middleware functions

---
+++ server/middleware/index.js
@@ -0,0 +1,6 @@
+// TODO: Middleware  functions:
+
+// // * verifyUser: that checks the user's token (verifies the request is coming from the user)
+// // * checkRole: checks that the user making the request has the right role
+// // * validation: makes sure that info being passed to database is correct
+// // * logger??? for error handling???

583f0cbe055b3f5a4c26bab243a968f88a2139db
Alex Grimes <107214142+agrimes23@users.noreply.github.com>
2023-10-15 17:42:31 -0500

    yml file (#47)
    * testing
    
    * tried husky, couldn't get it to work with nested fodlers. Will try github actions again
    
    * commented out check-localhost file
    
    * added a line of code to yml file, moved github workflow back to root folder
    
    * deleted workflow files inside client directory
    
    * testing url again

---
+++ client/check-localhost.js
@@ -0,0 +1,12 @@
+// const fs = require('fs');
+
+// const filePath = 'src/URL.jsx';
+// const fileContent = fs.readFileSync(filePath, 'utf8');
+
+// if (fileContent.includes('localhost') && !fileContent.includes('// const URL = "http://localhost:3001"')) {
+//   console.error('!! PLEASE FIX !!: Uncommented localhost reference found.');
+//   process.exit(1);
+// } else {
+//   console.log('No uncommented localhost references found.');
+//   process.exit(0);
+// }

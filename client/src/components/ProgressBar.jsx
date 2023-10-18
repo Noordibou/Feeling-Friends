e6929ebda1114f33ab231079a0c4c51003d5f31d
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-10-18 15:12:57 -0500

    checked dev tools and accessibility seems to be at 100, trying to improve performance

--- client/src/components/ProgressBar.jsx
+++ client/src/components/ProgressBar.jsx
@@ -14,7 +14,7 @@ const ProgressBar = ({ totalPages, currentPage }) => {
   return (
     <>
         <div className="flex w-11/12 max-w-2xl ">
-            <img className="ml-5" src={backArrow} onClick={goBack} />
+            <img className="ml-5 w-[25px] h-[34px]" alt="back arrow" src={backArrow} onClick={goBack} />
             {/* outer progress bar */}
             <div className="border-4 border-sandwich ml-12 w-3/4 h-8 rounded-2xl flex items-center">
                 {/* inner progress bar */}

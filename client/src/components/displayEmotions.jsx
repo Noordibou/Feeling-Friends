ca348279c89184ece6beee54fb34d42e29e5144f
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-10-28 15:22:16 -0500

    writing out the plan for sub emotion dynamic component display update

--- client/src/components/displayEmotions.jsx
+++ client/src/components/displayEmotions.jsx
@@ -2,6 +2,10 @@ import { Link } from "react-router-dom"
 
 const displayEmotions = () => {
 
+    // have an array of objects for each emotion (6) and the associated imgs
+    // loop through each object, and if the param is equal to what the user chose, keep it out from the buttons at the bottom
+    // do the find() js method for the main wheel
+
 
     return (
         <>

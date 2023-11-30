fe5580b95faf2a8980608899fad324df2a31e1f6
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-11-30 12:20:30 -0600

    added enums for furniture names

--- server/models/Teacher.js
+++ server/models/Teacher.js
@@ -1,32 +1,48 @@
 const mongoose = require("mongoose");
 
-
 const teacherSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   prefix: { type: String },
   avatarImg: { type: String },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   schoolTeacherId: { type: String },
-  classrooms: [{
-    classSubject: { type: String },
-    location: { type: String },
-    furniture: [{
-      name: { type: String },
-      x: { type: Number, default: null },
-      y: { type: Number, default: null },
-      assigned: { type: Boolean, default: false },
-    }],
-    students: [{ 
-      _id: false,
-      student: { type: mongoose.Schema.Types.ObjectId, ref: 'Student' },
-      seatInfo: {
-        x: { type: Number, default: null },
-        y: { type: Number, default: null },
-        assigned: { type: Boolean, default: false },
-      },
-     }],
-  }],
+  classrooms: [
+    {
+      classSubject: { type: String },
+      location: { type: String },
+      furniture: [
+        {
+          name: {
+            type: String,
+            enum: [
+              "Smartboard",
+              "Teacher's Desk",
+              "Door",
+              "Window",
+              "Empty Desk",
+              "Table",
+              "Bookcase/Storage",
+            ],
+          },
+          x: { type: Number, default: null },
+          y: { type: Number, default: null },
+          assigned: { type: Boolean, default: false },
+        },
+      ],
+      students: [
+        {
+          _id: false,
+          student: { type: mongoose.Schema.Types.ObjectId, ref: "Student" },
+          seatInfo: {
+            x: { type: Number, default: null },
+            y: { type: Number, default: null },
+            assigned: { type: Boolean, default: false },
+          },
+        },
+      ],
+    },
+  ],
 });
 
 module.exports = mongoose.model("Teacher", teacherSchema);

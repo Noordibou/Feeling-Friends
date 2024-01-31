6be08065126e553b2349a463cbd0e89f8bb2cc85
nd <noordibou.nd@gmail.com>
2024-01-30 19:29:49 -0600

    test

--- client/src/utils/sortByCriteria.js
+++ client/src/utils/sortByCriteria.js
@@ -1,38 +1,38 @@
-const sortByCriteria = (students, sortCriteria, sortDirection) => {
+// const sortByCriteria = (students, sortCriteria, sortDirection) => {
 
-    const zorOrder = ['Unmotivated', 'Wiggly', 'Ready to Learn', 'Explosive'];
+//     const zorOrder = ['Unmotivated', 'Wiggly', 'Ready to Learn', 'Explosive'];
 
-    const sortedStudents = [...students].sort((a, b) => {
-        if (sortCriteria === 'lastName') {
-            return sortDirection === 'asc'
-                ? a.lastName.localeCompare(b.lastName)
-                : b.lastName.localeCompare(a.lastName);
-        } else if (sortCriteria === 'zor') {
-            const zorA = a.journalEntries[a.journalEntries.length - 1]?.checkout?.ZOR || a.journalEntries[a.journalEntries.length - 1]?.checkin?.ZOR;
-            const zorB = b.journalEntries[b.journalEntries.length - 1]?.checkout?.ZOR || b.journalEntries[b.journalEntries.length - 1]?.checkin?.ZOR;
+//     const sortedStudents = [...students].sort((a, b) => {
+//         if (sortCriteria === 'lastName') {
+//             return sortDirection === 'asc'
+//                 ? a.lastName.localeCompare(b.lastName)
+//                 : b.lastName.localeCompare(a.lastName);
+//         } else if (sortCriteria === 'zor') {
+//             const zorA = a.journalEntries[a.journalEntries.length - 1]?.checkout?.ZOR || a.journalEntries[a.journalEntries.length - 1]?.checkin?.ZOR;
+//             const zorB = b.journalEntries[b.journalEntries.length - 1]?.checkout?.ZOR || b.journalEntries[b.journalEntries.length - 1]?.checkin?.ZOR;
 
-            const indexA = zorOrder.indexOf(zorA);
-            const indexB = zorOrder.indexOf(zorB);
+//             const indexA = zorOrder.indexOf(zorA);
+//             const indexB = zorOrder.indexOf(zorB);
 
 
-            if (indexA === -1 && indexB === -1) {
-                return 0;
-            } else if (indexA === -1) {
-                return 1;
-            } else if (indexB === -1) {
-                return -1;
-            } else {
-                return indexA - indexB;
-            }
-        }
+//             if (indexA === -1 && indexB === -1) {
+//                 return 0;
+//             } else if (indexA === -1) {
+//                 return 1;
+//             } else if (indexB === -1) {
+//                 return -1;
+//             } else {
+//                 return indexA - indexB;
+//             }
+//         }
 
-        return 0;
-    });
+//         return 0;
+//     });
 
-    if (sortCriteria === 'zor' && sortDirection === 'desc') {
-        sortedStudents.reverse();
-    }
-    return sortedStudents;
-};
+//     if (sortCriteria === 'zor' && sortDirection === 'desc') {
+//         sortedStudents.reverse();
+//     }
+//     return sortedStudents;
+// };
 
-module.exports = { sortByCriteria };
+// module.exports = { sortByCriteria };

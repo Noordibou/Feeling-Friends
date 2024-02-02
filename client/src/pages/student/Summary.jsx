21505893156e66b34aa74d1433f53ee8224ed0df
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2024-02-02 17:23:04 -0600

    add h2 tag instead of span, cleaned up a comment

--- client/src/pages/student/Summary.jsx
+++ client/src/pages/student/Summary.jsx
@@ -1,15 +1,13 @@
 import React, { useRef, useEffect, useState, useContext } from "react";
 import SummaryPerson from "../../images/SummaryPerson.png";
 import { AuthContext } from "../Authentication/AuthContext";
-import emotionsExplained from '../../data/emotionData.js';
+import emotionsExplained from "../../data/emotionData.js";
 import { useLocation } from "react-router-dom";
-import QuestionFrog from '../../images/Question_Frog.png';
-import { useUser } from '../../context/UserContext';
-import Logout from '../../components/LogoutButton.jsx'
+import QuestionFrog from "../../images/Question_Frog.png";
+import { useUser } from "../../context/UserContext";
+import Logout from "../../components/LogoutButton.jsx";
 import ProgressBar from "../../components/ProgressBar";
-import subEmotionInfo from "../../data/subEmotions.js"
-
-// TODO: have it say a message based on whether they've selected check-in or check-out (currently line 70)
+import subEmotionInfo from "../../data/subEmotions.js";
 
 const Summary = () => {
   const auth = useContext(AuthContext);
@@ -19,21 +17,21 @@ const Summary = () => {
   const [emotion, setEmotion] = useState("");
   const location = useLocation();
   const emotionFromLocation = location.state?.emotion || "";
-  const bottomContentRef = useRef()
-  const [mainEmotion, setMainEmotion] = useState("")
+  const bottomContentRef = useRef();
+  const [mainEmotion, setMainEmotion] = useState("");
 
   useEffect(() => {
     window.scrollTo(0, 0);
-    setMainEmotion(findMainEmotion(emotionFromLocation))
+    setMainEmotion(findMainEmotion(emotionFromLocation));
   }, [userData]);
-  
+
   useEffect(() => {
     const emotionFromParams = location.state?.emotion;
     if (emotionFromParams) {
       setEmotion(emotionFromParams);
     }
     console.log("Emotion:", emotion);
-  }, [emotion, location.state]); 
+  }, [emotion, location.state]);
 
   const findMainEmotion = (subemotion) => {
     for (let emotionObject of subEmotionInfo) {
@@ -42,7 +40,7 @@ const Summary = () => {
       }
     }
     return null;
-  }
+  };
 
   const getEmotionTips = () => {
     const emotionObject = emotionsExplained.find(
@@ -59,33 +57,42 @@ const Summary = () => {
     <>
       <div className="pt-[4rem] min-h-screen">
         <div className="flex w-full justify-center mb-20">
-          <ProgressBar totalPages="6" currentPage="6"/>
+          <ProgressBar totalPages="6" currentPage="6" />
         </div>
 
         <section>
           <div className="flex items-center justify-center">
-            <img className="h-42 w-82" src={SummaryPerson} alt="SummaryPerson"  />
+            <img
+              className="h-42 w-82"
+              src={SummaryPerson}
+              alt="SummaryPerson"
+            />
           </div>
 
           <div className="w-9/12 text-center ml-auto mr-auto pt-[1.5rem]">
             <h1 className="font-header1 text-header1 leading-tight">
-              {userData ? ("Thanks " + userData.firstName) : "Thanks!"} - Have a good day at school!
+              {userData ? "Thanks " + userData.firstName : "Thanks!"} - Have a
+              good day at school!
             </h1>
           </div>
           <div className="flex justify-center mt-24 mb-32">
-            <Logout location="studentLogout"/>
+            <Logout location="studentLogout" />
           </div>
         </section>
 
         {/* bottom orange section */}
-        <div className=" bg-lightOrange w-full h-8/12 rounded-[2rem] p-[2rem] flex flex-col
-             rounded-b">
-        
-        {/* Learn more */}
+        <div
+          className=" bg-lightOrange w-full h-8/12 rounded-[2rem] p-[2rem] flex flex-col
+             rounded-b"
+        >
+          {/* Learn more */}
           <section className="flex flex-row justify-around mt-2">
-              <div className="w-8/12">
-                <h2 className="text-[1.75rem] font-semibold font-karla">Getting to know our emotions can help with school success</h2>
-                <h2 className="underline text-header2 font-karla font-body font-semibold mt-7"
+            <div className="w-8/12">
+              <h2 className="text-[1.75rem] font-semibold font-karla">
+                Getting to know our emotions can help with school success
+              </h2>
+              <h2
+                className="underline text-header2 font-karla font-body font-semibold mt-7"
                 onClick={() => {
                   if (bottomContentRef.current) {
                     window.scrollTo({
@@ -94,38 +101,53 @@ const Summary = () => {
                     });
                   }
                 }}
-                >Learn more</h2>
-              </div>
-              <div className="">
-                <img src={QuestionFrog} alt="Avatar" className="w-full h-full" />
-              </div>
+              >
+                Learn more
+              </h2>
+            </div>
+            <div className="">
+              <img src={QuestionFrog} alt="Avatar" className="w-full h-full" />
+            </div>
           </section>
 
           {/* emotion explanation */}
           <section className="mt-40 mb-20">
             <div className="flex flex-col items-center">
-              
-
-                {/* Sub emotion transition sentence */}
-                { mainEmotion.toLowerCase() === "sad" ||
-                mainEmotion.toLowerCase() === "angry" ||
-                mainEmotion.toLowerCase() === "scared" ?
-                <div>
-                  <h2 className="font-header2 md:text-[2.5rem] text-md px-5">Being {emotionFromLocation.toLowerCase()} can feel like a big feeling that's hard to control.</h2>
-                  <h2 className="font-header2 md:text-[2.5rem] text-md px-5">What can we do when we feel {emotionFromLocation.toLowerCase()}?</h2>
-                </div>
-                : mainEmotion.toLowerCase() === "happy" ?
+              {/* Sub emotion transition sentence */}
+              {mainEmotion.toLowerCase() === "sad" ||
+              mainEmotion.toLowerCase() === "angry" ||
+              mainEmotion.toLowerCase() === "scared" ? (
                 <div>
                   <h2 className="font-header2 md:text-[2.5rem] text-md px-5">
-                    Being {emotionFromLocation.toLowerCase()} is the best! What can we do when we feel {emotionFromLocation.toLowerCase()}?
+                    Being {emotionFromLocation.toLowerCase()} can feel like a
+                    big feeling that's hard to control.
+                  </h2>
+                  <h2 className="font-header2 md:text-[2.5rem] text-md px-5">
+                    What can we do when we feel{" "}
+                    {emotionFromLocation.toLowerCase()}?
                   </h2>
                 </div>
-                :
-                <span>Feeling {emotionFromLocation.toLowerCase()} can help you do really cool things. What can we do when we feel {emotionFromLocation.toLowerCase()}?</span>
-                }
-                
-              
-              <ul ref={bottomContentRef} className="font-body leading-relaxed w-8/12 flex flex-col justify-center mt-5">
+              ) : mainEmotion.toLowerCase() === "happy" ? (
+                <div>
+                  <h2 className="font-header2 md:text-[2.5rem] text-md px-5">
+                    Being {emotionFromLocation.toLowerCase()} is the best! What
+                    can we do when we feel {emotionFromLocation.toLowerCase()}?
+                  </h2>
+                </div>
+              ) : (
+                <div>
+                  <h2 className="font-header2 md:text-[2.5rem] text-md px-5">
+                    Feeling {emotionFromLocation.toLowerCase()} can help you do
+                    really cool things. What can we do when we feel{" "}
+                    {emotionFromLocation.toLowerCase()}?
+                  </h2>
+                </div>
+              )}
+
+              <ul
+                ref={bottomContentRef}
+                className="font-body leading-relaxed w-8/12 flex flex-col justify-center mt-5"
+              >
                 {getEmotionTips().map((tip, index) => (
                   <li className="list-disc text-sm mt-[1rem]" key={index}>
                     {tip}
@@ -138,6 +160,6 @@ const Summary = () => {
       </div>
     </>
   );
-}
+};
 
 export default Summary;

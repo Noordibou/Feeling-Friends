3f3e21240dcfe322fb3c29d8ba8adf5a149c86ab
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2024-02-16 12:06:32 -0600

    still working on automation

--- server/cron/scheduleJETask.js
+++ server/cron/scheduleJETask.js
@@ -1,8 +1,10 @@
 const cron = require('cron')
 const { generateRandAnswers } = require('./generateRandAnswers')
 
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 // TODO: need to loop through all the students, update their JE array with new randomly generated answers.
-const task = cron.job('0 7 * * *', () => {
+const task = cron.job('0 7 * * 1,4', () => {
     const randomAnswers = generateRandAnswers();
 
 

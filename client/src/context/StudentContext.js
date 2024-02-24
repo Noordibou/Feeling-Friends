57326e744f04a9b88ec533f5aa38db7f48139984
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2024-02-24 07:52:51 -0600

    deleted unused student context. (only using userContext)

--- client/src/context/StudentContext.js
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from 'react';
-
-const StudentContext = createContext();
-
-export const useStudent = () => useContext(StudentContext);
-
-export default StudentContext;

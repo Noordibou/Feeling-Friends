24394104610eae9155e28201700918ccf5c10b07
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2024-03-02 13:41:44 -0600

    fixed a few utils functions to get current day last entry, not just last entry in general

--- client/src/utils/dateFormat.js
+++ client/src/utils/dateFormat.js
@@ -27,9 +27,21 @@ function calculateAge(birthday) {
     return formattedDate;
   };
 
+  // get current date format just as it is saved in the backend (2-12-2024)
+  // // the above date formatter uses 2/12/2024 format
+  function getCurrentDate() {
+    const today = new Date();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    const year = today.getFullYear();
+    
+    return `${month}-${day}-${year}`;
+  }
+
   module.exports ={
     calculateAge,
-    formatDate
+    formatDate,
+    getCurrentDate
   }
 
 
--- client/src/utils/editSeatChartUtil.js
+++ client/src/utils/editSeatChartUtil.js
@@ -1,13 +1,5 @@
 import { getBackgroundColorClass } from "./classroomColors";
-
-function getCurrentDate() {
-  const today = new Date();
-  const month = String(today.getMonth() + 1).padStart(2, '0');
-  const day = String(today.getDate()).padStart(2, '0');
-  const year = today.getFullYear();
-  
-  return `${month}-${day}-${year}`;
-}
+import { getCurrentDate } from "./dateFormat";
 
 export function getLastJournalInfo(student) {
   // returns if student has 0 journal entries

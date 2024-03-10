d119e7cf0f022fd0c384411e5541bfeea33289b1
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2024-03-10 17:00:19 -0500

    set up google analytics successfully

--- client/src/URL.jsx
+++ client/src/URL.jsx
@@ -1,4 +1,4 @@
-// const URL = "http://localhost:3001"
-const URL = "https://mindful-journal-server.vercel.app"
+const URL = "http://localhost:3001"
+// const URL = "https://mindful-journal-server.vercel.app"
 
 export default URL;

4914b535ea2f3969e658663d690521d50e28f111
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2024-03-17 13:04:56 -0500

    cleaning up the file

--- client/src/components/WeekView.jsx
+++ client/src/components/WeekView.jsx
@@ -8,44 +8,29 @@ const WeekView = ({ events, handleDateClick }) => {
   const [visibleDates, setVisibleDates] = useState(getVisibleDates());
   function getVisibleDates() {
     let today = new Date();
-    console.log("today: " + today);
     let dayOfWeek = today.getDay();
-    console.log("day of week: " + dayOfWeek);
      
     // Calculate last Monday
     let daysToLastMonday = (dayOfWeek + 6) % 7;    
     let lastMonday = new Date(today);
     lastMonday.setDate(today.getDate() - daysToLastMonday);
     lastMonday.setHours(0, 0, 0, 0); // Set time to midnight
-    console.log("Last Monday:", lastMonday);
     
     // Calculate this Sunday
     let daysToThisSunday = (7 - dayOfWeek) % 7;
     let thisSunday = new Date(today);
     thisSunday.setDate(today.getDate() + daysToThisSunday);
     thisSunday.setHours(23, 59, 59, 999); // Set time to end of day
-    console.log("This Sunday:", thisSunday);
     
     return { start: lastMonday, end: thisSunday };
   }
 
-  // function handleDateChange(date) {
-  //   setVisibleDates(getVisibleDates(date));
-  // }
-
   function isDateVisible(date) {
-    console.log("other dates??: "+ JSON.stringify(date))
-    if(date >= visibleDates.start && date <= visibleDates.end) {
-      // console.log("date: "+ JSON.stringify(date))
-      console.log("visibleDates: "+ JSON.stringify(visibleDates))
-      return date >= visibleDates.start && date <= visibleDates.end;
-    }
-    
+    return date >= visibleDates.start && date <= visibleDates.end;    
   }
 
   useEffect(() => {
     getVisibleDates();
-    console.log("visible dates" + JSON.stringify(visibleDates))
   }, [])
 
   return (

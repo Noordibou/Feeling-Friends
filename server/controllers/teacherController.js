7302b9360319a72c7a0830a8d46f3354e97da08a
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2024-03-27 14:29:33 -0500

    forgot to save this one, deleted unnecessary console logs

--- server/controllers/teacherController.js
+++ server/controllers/teacherController.js
@@ -454,7 +454,6 @@ const addFurniture = async (req, res) => {
 }
 
 const updateFurniturePositions = async (req, res) => {
-  console.log("Oh heyy it hit the backend update furniture positions hmm")
   try {
     const teacherId = req.params.id;
     const classroomId = req.params.classroomId;
@@ -494,13 +493,10 @@ const updateFurniturePositions = async (req, res) => {
 };
 
 const deleteFurniture = async (req, res) => {
-  console.log("oh hi")
   try {
     const teacherId = req.params.id;
     const classroomId = req.params.classroomId;
     const itemIdsToDelete = req.body;
-    console.log("what is this: " + JSON.stringify(itemIdsToDelete))
-    console.log("req.body: " + JSON.stringify(req.body))
     const teacher = await Teacher.findById(teacherId);
 
     if (!teacher) {
@@ -512,7 +508,6 @@ const deleteFurniture = async (req, res) => {
     if (!classroom) {
       return res.status(404).json({ error: "Classroom not found" });
     }
-    console.log("okay here")
     // Find the furniture item to delete
     itemIdsToDelete.forEach(itemId => {
       // Find the furniture item to delete

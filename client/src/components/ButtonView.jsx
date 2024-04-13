975ff709cf8f565aefe80c299c3d1ea34bdb7339
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2024-04-12 20:04:52 -0500

    fixed the classroom nav bar component's styling, and updated img size for buttonView

--- client/src/components/ButtonView.jsx
+++ client/src/components/ButtonView.jsx
@@ -17,13 +17,14 @@ const ButtonView = ({
     <>
       <div className={`${buttonSize !== "small" ? "mx-2" : ""}`}>
         <button
-          className={`text-[16px]  text-[Poppins] rounded-xl flex ${flexType} items-center ${buttonSize === "small" ? "pt-3 h-24 w-28" : "h-20 w-[270px]" }  border-[4px] border-sandwich justify-around ${
+          className={`text-[16px]  text-[Poppins] rounded-xl flex ${flexType} items-center ${buttonSize === "small" ? "pt-3 h-24 w-28" : "h-20 w-[270px] border-[4px] border-sandwich" }  justify-around ${
             isSelected ? "underline font-[700] bg-sandwich" : "bg-notebookPaper"
           }`}
           onClick={handleClick}
         >
           <h4 className="">{buttonText}</h4>
           <img
+          className="w-10"
             src={!isSelected ? defaultBtnImage : btnImageWhenOpen}
             alt={buttonText}
           />

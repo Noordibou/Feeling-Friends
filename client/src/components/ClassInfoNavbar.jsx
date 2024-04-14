7bae53f5d62b6384dbca9325d223110f4ab808ac
Alex Grimes <107214142+agrimes23@users.noreply.github.com>
2024-04-14 12:50:24 -0500

    Desktop class (#142)
    * updated classroom info successfully
    
    * updated bold on class navbar, deleted some comments
    
    * reverted changes due to deployment issue, checking changes file by file. This update is the styling on student info cards
    
    * updated save button styling in class list page
    
    * added util function
    
    * found the issue is with the new util function, changed the way it's being exported
    
    * last commit succeed deployment, added rest of the changes by importing formatTime util
    
    * pull from dev, updated teacher nav bar with previous functionality

--- client/src/components/ClassInfoNavbar.jsx
+++ client/src/components/ClassInfoNavbar.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import GoBack from "./GoBack";
 import { getTeacherClassroom } from "../api/teachersApi";
+import { formatTime } from "../utils/dateFormat";
 
 const ClassInfoNavbar = ({ teacherId, classroomId }) => {
   const [classroom, setClassroom] = useState("");
@@ -22,7 +23,7 @@ const ClassInfoNavbar = ({ teacherId, classroomId }) => {
     <div className="flex items-center justify-around mb-[0.5rem] mt-10 w-full ">
       <div className="flex items-center ">
         <GoBack />
-        <h2 className="text-[25px] ml-[2rem] ">
+        <h2 className="text-[25px] ml-[2rem] font-semibold">
           {classroom.classSubject}
         </h2>
       </div>
@@ -35,11 +36,11 @@ const ClassInfoNavbar = ({ teacherId, classroomId }) => {
         <div className="flex gap-4">
           <div className="border-l-4 pl-4 border-sandwich ">
             <h2>Check-in</h2>
-            <h2>{classroom.checkIn ? `${classroom.checkIn}AM` : "-"}</h2>
+            <h2 className="font-semibold">{classroom.checkIn ? formatTime(classroom.checkIn) : "-"}</h2>
           </div>
           <div className="border-r-4 pr-4 border-sandwich ">
             <h2>Check-out</h2>
-            <h2>{classroom.checkOut ? `${classroom.checkOut}PM` : "-"}</h2>
+            <h2 className="font-semibold">{classroom.checkOut ? formatTime(classroom.checkOut) : "-"}</h2>
           </div>
         </div>
       </div>

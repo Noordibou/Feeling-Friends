a6c5cca6c92ef4e2b9f998d2432ba19807c0948a
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2024-04-14 12:45:26 -0500

    pull from dev, updated teacher nav bar with previous functionality

--- client/src/components/TeacherNavbar.jsx
+++ client/src/components/TeacherNavbar.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import Exterior from "../images/Exterior.png";
 import Classroom from "../images/Classroom.png";
 import Goal from "../images/Goal.png";
 import Settings from "../images/Settings.png";
 
-export default function TeacherNavbar() {
+export default function TeacherNavbar({ setIsEditMode, teacherId, classroomId }) {
+  const [isEditMode, setEditMode] = useState(false);
   // Function to generate tab style based on whether it's active
   const getTabStyle = ({ isActive }) => {
     return `font-poppins text-notebookPaper text-md nav-text-shadow ${isActive ? 'underline' : ''}`;
+  };
+
+  const redirectTo = (url) => {
+    window.location.href = url;
   };
 
   const handleEditClick = () => {
@@ -17,11 +22,14 @@ export default function TeacherNavbar() {
 
     // Check if the current page is the classroom page
     if (window.location.pathname === classroomPageUrl) {
+      setEditMode(!isEditMode)
       redirectTo(`/edit-seating-chart/${teacherId}/${classroomId}`);
     } else if (supportsEditMode) {
       // Toggle edit mode using the function passed down as a prop
+      setEditMode(!isEditMode)
       setIsEditMode((prevEditMode) => !prevEditMode);
     } else {
+      setEditMode(!isEditMode)
       // Display a message or perform some other action when edit mode is not supported
       alert("This page does not support edit mode.");
     }
@@ -37,9 +45,11 @@ export default function TeacherNavbar() {
       
 
       
-        <div className="bg-grass w-1/4 flex flex-col items-center border-[1rem] border-grass font-semibold">
-          <NavLink to="/teacher-home" className={getTabStyle}><div className="flex justify-center items-center"><img src={Classroom} alt="Classroom" /></div>
-          <div>Edit Classes</div></NavLink>
+        <div className={`bg-grass w-1/4 flex flex-col items-center border-[1rem] border-grass font-semibold ${isEditMode ? 'edit-mode underline' : ''}`} onClick={handleEditClick}>
+        <NavLink className={getTabStyle}>
+          <div className="flex justify-center items-center"><img src={Classroom} alt="Classroom" /></div>
+          <div>Edit Classes</div>
+          </NavLink>
         </div>
       
 

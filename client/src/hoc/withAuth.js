cae9a9707aee956c44c5fc8dca7e7fd48c8a2691
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2024-06-23 19:57:12 -0500

    if an authenticated user tries to navigate to a url that doesnt match the role, they are redirect to respective home page

--- client/src/hoc/withAuth.js
+++ client/src/hoc/withAuth.js
@@ -1,35 +1,54 @@
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import axios from 'axios'
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const withAuth = (allowedRoles) => (WrappedComponent) => {
+  const WrapperComponent = (props) => {
+    const navigate = useNavigate();
+    const [isAuthorized, setIsAuthorized] = useState(null);
 
-    const WrapperComponent = (props) => {
-        const navigate = useNavigate()
-    
-        useEffect(() => {
-           const checkAuth = async () => {
-                try {
-                    const response = await axios.get(`${process.env.REACT_APP_URL}/check-auth`, { withCredentials: true })
-                    if (response.data.status && allowedRoles.includes(response.data.role)) {
-                        console.log("continue..")
-                    } else {
-                        console.log("cannot find token or user is not verified for this login")
-                        navigate("/login")
-                    }
-                } catch (error) {
-                    console.error('Authentication error:', error);
-                    navigate('/login');
-                }
-           }
+    useEffect(() => {
+      const checkAuth = async () => {
+        try {
+          // check for authentication
+          const response = await axios.get(
+            `${process.env.REACT_APP_URL}/check-auth`,
+            { withCredentials: true }
+          );
+          // if it returns as true, there's an authenticated token
+          if (response.data.status) {
+            const userRole = response.data.role;
+            // check for roles in the returned response
+            if (allowedRoles.includes(userRole)) {
+              setIsAuthorized(true)
+            } else {
+              if (userRole === "teacher") {
+                navigate("/teacher-home");
+              } else if (userRole === "student") {
+                navigate("/student-home")
+              } else {
+                navigate("/login")
+              }
+              setIsAuthorized(false)
+            }
+          } else {
+            // User is not authenticated
+            navigate("/login")
+            setIsAuthorized(false)
+          }
+        } catch (error) {
+          console.error("Authentication error:", error);
+          navigate("/login");
+        }
+      };
 
-           checkAuth()
-        }, [navigate])
+      checkAuth();
+    }, [navigate]);
 
-        return <WrappedComponent {...props} />
-    }
+    return <WrappedComponent {...props} />;
+  };
 
-  return WrapperComponent
-}
+  return WrapperComponent;
+};
 
-export default withAuth
+export default withAuth;

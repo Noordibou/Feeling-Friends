7ba1db8750ac19d251e835a796503ffa93cae9f5
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2024-06-24 21:53:03 -0500

    updated desktop styling of classInfoNavbar

--- client/src/components/ClassInfoNavbar.jsx
+++ client/src/components/ClassInfoNavbar.jsx
@@ -23,25 +23,36 @@ const ClassInfoNavbar = ({ teacherId, classroomId }) => {
     <div className="flex items-center justify-around mb-[0.5rem] mt-10 w-full ">
       <div className="flex items-center ">
         <GoBack />
-        <h2 className="text-[25px] ml-[2rem] font-semibold">
+        <h2 className="text-[24px] ml-[2rem] font-semibold">
           {classroom.classSubject}
         </h2>
       </div>
-      <div className="flex-col text-xs font-body border-l-4 pl-4 border-sandwich ">
-        <h2>Location:</h2>
-        <h2 className="font-semibold">{classroom.location}</h2>
-      </div>
 
-      <div className="flex-col text-xs font-body">
-        <div className="flex gap-4">
-          <div className="border-l-4 pl-4 border-sandwich ">
-            <h2>Check-in</h2>
-            <h2 className="font-semibold">{classroom.checkIn ? formatTime(classroom.checkIn) : "-"}</h2>
+      {/* class details */}
+      <div className="flex-col text-xs font-body border-l-4 pl-4 border-sandwich pr-4 border-r-4">
+        <div className="flex py-[2px] text-[14px]">
+          <h2>Location:</h2>
+          <h2 className="font-semibold pl-2 text-graphite">{classroom.location}</h2>
+        </div>
+
+        {/* check in/out section */}
+        <div className="flex py-[2px] text-[14px]">
+          <div className="flex">
+            <h2>Check-in: </h2>
+            <h2 className="font-semibold pl-2 text-graphite">
+              {classroom.checkIn ? formatTime(classroom.checkIn) : "-"}
+            </h2>
           </div>
-          <div className="border-r-4 pr-4 border-sandwich ">
-            <h2>Check-out</h2>
-            <h2 className="font-semibold">{classroom.checkOut ? formatTime(classroom.checkOut) : "-"}</h2>
+          <div className="flex pl-4 text-[14px]">
+            <h2>Check-out:</h2>
+            <h2 className="font-semibold pl-2 text-graphite">
+              {classroom.checkOut ? formatTime(classroom.checkOut) : "-"}
+            </h2>
           </div>
+        </div>
+        <div className="flex py-[2px] text-[14px]">
+          <h2>Days:</h2>
+          <h2 className="font-semibold pl-2 tracking-wide text-graphite">MON | TUE | WED | THU | FRI</h2>
         </div>
       </div>
     </div>

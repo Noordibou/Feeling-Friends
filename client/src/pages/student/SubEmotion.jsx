517e91c71ac111f12f404e0e1fe1b86c143b6b11
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2024-07-04 15:01:56 -0500

    updated styling for mobile for sub emotion screen. Still need to figure out how to style the words on the sub emotion wheel

--- client/src/pages/student/SubEmotion.jsx
+++ client/src/pages/student/SubEmotion.jsx
@@ -52,58 +52,60 @@ const SubEmotion = () => {
 
   return (
     <div className="flex flex-col items-center h-screen min-w-screen ">
-      <div className="flex w-full justify-center mt-20">
+      <div className="flex w-full justify-center mt-10 sm:mt-20">
         <ProgressBar totalPages="5" currentPage="2" />
       </div>
-      <div className="font-header2 text-header2 text-center pt-8 ">
+      <div className="font-[Karla] text-[22px] font-semibold sm:font-header2 sm:text-header2 text-center pt-8 ">
           <h2>Choose the emotion closest to</h2>
           <span>what you're feeling.</span>
         </div>
-      <div className="flex flex-col text-center mt-10 h-full w-full items-center">
-        
-        <div className="relative font-header3 mt-5 md:text-header3 text-[1.1rem] ">
-          <img
-            src={wheelImg}
-            alt="emotions wheel"
-            className="w-[37rem] h-[35rem] object-fill"
-          />
-          <div className="absolute right-[12.5rem] -top-20 inset-0 flex flex-col items-center justify-center text-center text-body">
-            {subEmotions.map((subEmotion, index) => {
-              const angle = angleBetweenButtons * index;
-              const radius = 13; // Adjust the radius as needed
-              const x = radius * Math.cos(angle);
-              const y = radius * Math.sin(angle);
+        {/* sub emotion and other emotions */}
+      <div className="flex flex-col text-center sm:mt-10 h-full w-full items-center">
+        {/* sub emotions only */}
+        <div className="relative font-header3 mt-5 sm:text-header3 text-[1.1rem]">
+  <img
+    src={wheelImg}
+    alt="emotions wheel"
+    className="w-[20rem] h-[20rem] sm:w-[37rem] sm:h-[35rem] object-fill"
+  />
+  <div className="absolute right-28 sm:right-[12.5rem] -top-10 sm:-top-20 inset-0 flex flex-col items-center justify-center text-center text-body">
+    {subEmotions.map((subEmotion, index) => {
+      const angle = angleBetweenButtons * index;
+      const radius = 7; // Adjust the radius for small screens
+      const radiusMd = 13; // Adjust the radius for medium and larger screens
+      const x = radius * Math.cos(angle);
+      const y = radius * Math.sin(angle);
+      const xMd = radiusMd * Math.cos(angle);
+      const yMd = radiusMd * Math.sin(angle);
 
-              return (
-                <button
-                  key={index}
-                  onClick={() => handleEmotionClick(subEmotion)}
-                  className="cursor-pointer hover:font-semibold w-1/2 py-7 "
-                  style={{
-                    position: "absolute",
-                    left: `calc(50% + ${x}rem)`,
-                    top: `calc(50% + ${y}rem)`,
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                  }}
-                >
-                    {subEmotion}
-                </button>
-              );
-            })}
-          </div>
-        </div>
-        <div className="-mt-20 justify-center w-11/12 md:w-[750px]">
+      return (
+        <button
+          key={index}
+          onClick={() => handleEmotionClick(subEmotion)}
+          className={`absolute cursor-pointer text-[17px] hover:font-semibold w-1/2 py-3 sm:py-7 left-[calc(50% + ${x}rem)]`}
+          style={{
+            left: `calc(50% + ${x}rem)`,
+            top: `calc(50% + ${y}rem)`,
+          }}
+        >
+          {subEmotion}
+        </button>
+      );
+    })}
+  </div>
+</div>
+        
+        {/* other main emotions options */}
+        <div className="-mt-6 sm:-mt-20 justify-center w-11/12 md:w-[750px]">
           <div className="flex justify-between">
             <button
-              className="rounded-full w-24 h-24"
+              className="rounded-full w-16 h-16 sm:w-24 sm:h-2"
               onClick={() => changeEmotion(matchedEmotions[0].emotion)}
             >
               <img src={matchedEmotions[0].eImage} alt="" className="w-22 h-22 object-fill  " />
             </button>
             <button
-              className="rounded-full w-24 h-24"
+              className="rounded-full w-16 h-16 sm:w-24 sm:h-24"
               onClick={() => changeEmotion(matchedEmotions[1].emotion)}
             >
               <img src={matchedEmotions[1].eImage} alt="" className="w-22 h-22 object-fill " />
@@ -112,13 +114,13 @@ const SubEmotion = () => {
 
           <div className="flex justify-around">
             <button
-              className="rounded-full w-24 h-24"
+              className="rounded-full w-16 h-16 sm:w-24 sm:h-2"
               onClick={() => changeEmotion(matchedEmotions[2].emotion)}
             >
               <img src={matchedEmotions[2].eImage} alt="" className="w-22 h-22 object-fill " />
             </button>
             <button
-              className="rounded-full w-24 h-24 "
+              className="rounded-full w-16 h-16 sm:w-24 sm:h-2"
               onClick={() => changeEmotion(matchedEmotions[3].emotion)}
             >
               <img src={matchedEmotions[3].eImage} alt="" className="w-22 h-22 object-fill " />
@@ -127,7 +129,7 @@ const SubEmotion = () => {
 
           <div className="-mt-12 flex justify-center">
             <button
-              className="rounded-full w-24 h-24"
+              className="rounded-full w-16 h-16 sm:w-24 sm:h-2"
               onClick={() => changeEmotion(matchedEmotions[4].emotion)}
             >
               <img src={matchedEmotions[4].eImage} alt="" className="w-22 h-22 object-fill " />

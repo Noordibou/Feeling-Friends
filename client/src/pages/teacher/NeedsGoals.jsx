e95ba62c022639af16e96d224d1dda0d833de122
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2024-07-20 11:17:34 -0500

    added semi-functionality to needs section

--- client/src/pages/teacher/NeedsGoals.jsx
+++ client/src/pages/teacher/NeedsGoals.jsx
@@ -26,16 +26,22 @@ const NeedsGoals = () => {
   const [students, setStudents] = useState([]);
   const [goalAnswers, setGoalAnswers] = useState([""]);
   const [needAnswers, setNeedAnswers] = useState([""]);
-  const [editMode, setEditMode] = useState(
-    Array(goalAnswers.length).fill(false)
-  );
+  const [editGoalMode, setEditGoalMode] = useState(Array(goalAnswers.length).fill(false));
+  const [editNeedsMode, setEditNeedsMode] = useState(Array(needAnswers.length).fill(false));
+
 
 
   // keeps track of which lines are being edited
-  const toggleEditMode = (index) => {
-    const updatedEditMode = [...editMode];
-    updatedEditMode[index] = !updatedEditMode[index];
-    setEditMode(updatedEditMode);
+  const toggleEditGoalMode = (index) => {
+    const updatedEditGoalMode = [...editGoalMode];
+    updatedEditGoalMode[index] = !updatedEditGoalMode[index];
+    setEditGoalMode(updatedEditGoalMode);
+  };
+  
+  const toggleEditNeedsMode = (index) => {
+    const updatedEditNeedsMode = [...editNeedsMode];
+    updatedEditNeedsMode[index] = !updatedEditNeedsMode[index];
+    setEditNeedsMode(updatedEditNeedsMode);
   };
 
   // Can update these functions based on how backend is edited for goals and needs change
@@ -173,76 +179,71 @@ const NeedsGoals = () => {
 
             {/* Divs in place of buttons for this selection probably. Here is one div since they will probably need to be listed from the backend depending on how many choices the teacher has made */}
             {goalAnswers.map((answer, index) => (
-            
-
-                  <div
+              <div
+                key={index}
+                className={`flex bg-white rounded-[1rem] border-graphite border-[4px]  items-center justify-between mt-[1rem] mb-[1rem]`}
+              >
+                {editGoalMode[index] ? (
+                  <textarea
                     key={index}
-                    className={`flex bg-white rounded-[1rem] border-graphite border-[4px]  items-center justify-between mt-[1rem] mb-[1rem]`}
+                    value={answer}
+                    onChange={(e) =>
+                      handleInputGoalChange(index, e.target.value)
+                    }
+                    className="w-10/12 px-3 pt-[15px] md:px-5 rounded-[1rem] text-[17px] font-body "
+                  />
+                ) : (
+                  <div
+                    className={`flex rounded-[1rem] items-center justify-between mt-[1rem] mb-[1rem] h-[34px] `}
                   >
-                    {editMode[index] ? (
-                    <textarea
-                      key={index}
-                      value={answer}
-                      onChange={(e) =>
-                        handleInputGoalChange(index, e.target.value)
-                      }
-                      className="w-10/12 px-3 pt-[15px] md:px-5 rounded-[1rem] text-[17px] font-body "
-                    />
-                  ) : (
-                    <div
-                    className={`flex rounded-[1rem] items-center justify-between mt-[1rem] mb-[1rem] bg-green`}
-                  >
-                    <h3 className="text-[17px] font-body">
-                      Finish homework during study hall
-                    </h3>
+                    <h3 className="text-[17px] font-body pl-3">{answer}</h3>
                   </div>
+                )}
+                <div className="flex flex-col-reverse md:flex-row text-body font-body items-center pr-4">
+                  {/* edit button */}
+                  <button onClick={() => toggleEditGoalMode(index)}>
+                    <img
+                      className={` h-5 md:h-7 px-3 ${
+                        editGoalMode[index] ? "" : "opacity-50"
+                      }`}
+                      src={editIcon}
+                      alt="edit"
+                    />
+                  </button>
 
-                  )}
-                    <div className="flex flex-col-reverse md:flex-row text-body font-body items-center pr-2 w-2/12 gap-1 md:gap-0">
-                      {/* edit button */}
-                      <button onClick={() => toggleEditMode(index)}>
-                        <img className={` h-5 md:h-7 px-3 ${editMode[index] ?"" : "opacity-50"}`} src={editIcon} alt="edit" />
-                        
-                      </button>
-
-                      {/* delete "x" button */}
-                      <button onClick={() => removeGoalsAnswer(index)}>
-                        <svg
-                          width="24"
-                          height="24"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="w-[18px] sm:w-[24px]"
-                        >
-                          <line
-                            x1="5"
-                            y1="5"
-                            x2="19"
-                            y2="19"
-                            stroke="#000"
-                            strokeWidth="4"
-                            strokeLinecap="round"
-                          />
-                          <line
-                            x1="19"
-                            y1="5"
-                            x2="5"
-                            y2="19"
-                            stroke="#000"
-                            strokeWidth="4"
-                            strokeLinecap="round"
-                          />
-                        </svg>
-                      </button>
-                    </div>
-                    </div>
-                    
-                  
-                  
-                
-                  ))}
-
+                  {/* delete "x" button */}
+                  <button onClick={() => removeGoalsAnswer(index)}>
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-[18px] sm:w-[24px]"
+                    >
+                      <line
+                        x1="5"
+                        y1="5"
+                        x2="19"
+                        y2="19"
+                        stroke="#000"
+                        strokeWidth="4"
+                        strokeLinecap="round"
+                      />
+                      <line
+                        x1="19"
+                        y1="5"
+                        x2="5"
+                        y2="19"
+                        stroke="#000"
+                        strokeWidth="4"
+                        strokeLinecap="round"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            ))}
 
             {/* Add new goal div */}
             <div className="rounded-[1rem] border-graphite border-[4px] py-2 mt-[1rem] mb-[1.5rem]">
@@ -275,75 +276,87 @@ const NeedsGoals = () => {
             <h2 className="font-[Poppins] text-[18px] md:text-[22px] mb-6">
               "What do you <u>need</u> from an adult to succeed today?"
             </h2>
-            {/* {isEditMode ? (
-              <>
-                {needAnswers.map((answer, index) => (
-                  <div
-                    key={index}
-                    className={`flex bg-white rounded-[1rem] border-graphite border-[4px]  items-center justify-between mt-[1rem] mb-[1rem]`}
-                  >
-                    <textarea
-                      key={index}
-                      value={answer}
-                      onChange={(e) =>
-                        handleInputNeedChange(index, e.target.value)
-                      }
-                      className="w-full px-3 pt-[15px] md:px-5 rounded-[1rem] text-[17px] font-body "
-                    />
-                    <div className="flex text-body font-body items-center pr-4"> */}
-                      {/* delete "x" button */}
-                      {/* <button onClick={() => removeNeedsAnswer(index)}>
-                        <svg
-                          width="24"
-                          height="24"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="w-[18px] sm:w-[24px]"
-                        >
-                          <line
-                            x1="5"
-                            y1="5"
-                            x2="19"
-                            y2="19"
-                            stroke="#000"
-                            strokeWidth="4"
-                            strokeLinecap="round"
-                          />
-                          <line
-                            x1="19"
-                            y1="5"
-                            x2="5"
-                            y2="19"
-                            stroke="#000"
-                            strokeWidth="4"
-                            strokeLinecap="round"
-                          />
-                        </svg>
-                      </button>
-                    </div>
-                  </div>
-                ))} */}
-                {/* Add new need div */}
-                {/* <div className="rounded-[1rem] border-graphite border-[4px] py-2 mt-[1rem] mb-[1.5rem]">
-                  <h4
-                    className="text-[17px] font-semibold font-[Poppins] text-center"
-                    role="button"
-                    onClick={() => addAnswer("need")}
-                  >
-                    Add new need +
-                  </h4>
-                </div>
-              </>
-            ) : (
+
+            {needAnswers.map((answer, index) => (
               <div
-                className={`flex p-3 md:p-5 rounded-[1rem] border-graphite border-[4px]  items-center justify-between mt-[1rem] mb-[1rem]`}
+                key={index}
+                className={`flex bg-white rounded-[1rem] border-graphite border-[4px]  items-center justify-between mt-[1rem] mb-[1rem]`}
               >
-                <h3 className="text-[17px] font-body">
-                  Finish homework during study hall
-                </h3>
+                {editNeedsMode[index] ? (
+                  <textarea
+                    key={index}
+                    value={answer}
+                    onChange={(e) =>
+                      handleInputNeedChange(index, e.target.value)
+                    }
+                    className="w-10/12 px-3 pt-[15px] md:px-5 rounded-[1rem] text-[17px] font-body "
+                  />
+                ) : (
+                  <div
+                    className={`flex md:p-5 rounded-[1rem] items-center justify-between mt-[1rem] mb-[1rem] h-[34px] `}
+                  >
+                    <h3 className="text-[17px] font-body pl-3">
+                      {answer}
+                    </h3>
+                  </div>
+                )}
+
+                <div className="flex flex-col-reverse md:flex-row text-body font-body items-center pr-4">
+                  {/* edit button */}
+                  <button onClick={() => toggleEditNeedsMode(index)}>
+                    <img
+                      className={` h-5 md:h-7 px-3 ${
+                        editNeedsMode[index] ? "" : "opacity-50"
+                      }`}
+                      src={editIcon}
+                      alt="edit"
+                    />
+                  </button>
+
+                  {/* delete "x" button */}
+                  <button onClick={() => removeNeedsAnswer(index)}>
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-[18px] sm:w-[24px]"
+                    >
+                      <line
+                        x1="5"
+                        y1="5"
+                        x2="19"
+                        y2="19"
+                        stroke="#000"
+                        strokeWidth="4"
+                        strokeLinecap="round"
+                      />
+                      <line
+                        x1="19"
+                        y1="5"
+                        x2="5"
+                        y2="19"
+                        stroke="#000"
+                        strokeWidth="4"
+                        strokeLinecap="round"
+                      />
+                    </svg>
+                  </button>
+                </div>
               </div>
-            )} */}
+            ))}
+
+            {/* Add new need div */}
+            <div className="rounded-[1rem] border-graphite border-[4px] py-2 mt-[1rem] mb-[1.5rem]">
+              <h4
+                className="text-[17px] font-semibold font-[Poppins] text-center"
+                role="button"
+                onClick={() => addAnswer("need")}
+              >
+                Add new need +
+              </h4>
+            </div>
 
             <div className="flex mx-2 gap-5 items-center justify-center">
               <label
@@ -392,10 +405,7 @@ const NeedsGoals = () => {
       </div>
 
       <div className="bottom-0 z-40 fixed w-screen lg:inset-y-0 lg:left-0 lg:order-first lg:w-44 ">
-        <Nav
-          teacherId={teacherId}
-          classroomId={classroomId}
-        />
+        <Nav teacherId={teacherId} classroomId={classroomId} />
       </div>
     </>
   );

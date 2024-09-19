7617014062f8748b86f952fd9cb86f3ba363a450
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2024-09-18 21:54:31 -0500

    all tests work now as expected, and tried to convert the driver.sleep to selenium wait until as much as I could

--- client/src/tests/e2e/authentication/login.test.js
+++ client/src/tests/e2e/authentication/login.test.js
@@ -1,96 +1,138 @@
-const { Builder, By, until } = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
-const { login, deleteTeacherUser, signupNewUser, logout, expectFailMsg, getRandomString } = require('../utils.js');
+const { Builder, By, until } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+const {
+  login,
+  deleteTeacherUser,
+  signupNewUser,
+  logout,
+  expectLoginFail,
+  getRandomString,
+} = require("../utils.js");
 
 // Configure Chrome options for Selenium
 const chromeOptions = new chrome.Options();
-// chromeOptions.addArguments('--headless'); // Uncomment to run in headless mode for CI
+chromeOptions.addArguments('--headless');
 
 let driver;
-const emailString = getRandomString(6)
-const email = `test${emailString}@email.com`
-const password = 'Password1!';
+const emailString = getRandomString(6);
+const email = `test${emailString}@email.com`;
+const password = "Password1!";
 const testFirstname = getRandomString(6);
 const testLastname = getRandomString(6);
 const testUsername = getRandomString(8);
 
 // executes before all the tests
 beforeAll(async () => {
-    driver = await new Builder().forBrowser('chrome').setChromeOptions(chromeOptions).build();
-    await driver.get('http://localhost:3000/signup');
-    
-    // Pre-test setup: Sign up a new user
-    await signupNewUser(driver, testFirstname, testLastname, testUsername, email, password); 
-    await logout(driver)
-    await driver.sleep(500)
+  driver = await new Builder()
+    .forBrowser("chrome")
+    .setChromeOptions(chromeOptions)
+    .build();
+  await driver.get("http://localhost:3000/signup");
+
+  // Pre-test setup: Sign up a new user
+  await signupNewUser(
+    driver,
+    testFirstname,
+    testLastname,
+    testUsername,
+    email,
+    password
+  );
+  await driver.sleep(1000);
+  await logout(driver);
 });
 
 // executes after all tests
 afterAll(async () => {
-    try {
-        // Ensure the session is still valid before cleanup
-        if (driver) {
-            await deleteTeacherUser(driver);
-            await driver.get('http://localhost:3000/login');
-            await driver.sleep(500);
-            await login(driver, email, password)
-            await driver.sleep(500)
-            await expectFailMsg(driver, "Incorrect password or email");
-        }
-    } catch (err) {
-        console.error("Error during afterAll:", err);
-    } finally {
-        // Ensure driver is properly quit
-        if (driver) {
-            await driver.quit();
-        }
-    }
+  await login(driver, email, password);
+  await driver.sleep(500);
+  await deleteTeacherUser(driver);
+  await driver.get("http://localhost:3000/login");
+  await login(driver, email, password);
+  await driver.sleep(1000);
+  await expectLoginFail(driver, email, password);
+
+  // Ensure driver is properly quit
+  if (driver) {
+    await driver.quit();
+  }
 });
 
+jest.setTimeout(25000);
 
-jest.setTimeout(40000);
+test("Login flow and verify local storage and redirection", async () => {
+  // GIVEN (sets up the test)
+  await driver.get("http://localhost:3000/login");
+  // GIVEN, THEN, WHEN as comments for each section
+  // Login with the newly signed up user
+  await login(driver, email, password);
+//   await driver.sleep(500);
+//   await driver.navigate().refresh();
+  // Use explicit waits for teacher-home to load
+  await driver.wait(until.urlIs("http://localhost:3000/teacher-home"), 1000);
 
-test('Login flow and verify local storage and redirection', async () => {
-    // GIVEN (sets up the test)
-    await driver.get('http://localhost:3000/login');
-    // GIVEN, THEN, WHEN as comments for each section
-    // Login with the newly signed up user
-    await login(driver, email, password);
-    
-    // Use explicit waits for teacher-home to load
-    await driver.wait(until.urlIs('http://localhost:3000/teacher-home'), 1000);
+  // Check redirection
+  const currentUrl = await driver.getCurrentUrl();
+  expect(currentUrl).toBe("http://localhost:3000/teacher-home");
 
-    // Check redirection
-    const currentUrl = await driver.getCurrentUrl();
-    expect(currentUrl).toBe('http://localhost:3000/teacher-home');
-
-
-    const localStorageData = await driver.executeScript("return localStorage.getItem('userData');");
-    expect(localStorageData).not.toBeNull();
-    await driver.sleep(500);
+  const localStorageData = await driver.executeScript(
+    "return localStorage.getItem('userData');"
+  );
+  expect(localStorageData).not.toBeNull();
+  await driver.sleep(500);
 });
 
-test('Login fails and error toast appears with invalid credentials', async () => {
-    // GIVEN:
-    await driver.get('http://localhost:3000/login');
-    const invalidEmail = 'invalid-email@test.com';
-    const invalidPassword = 'InvalidPassword123';
+test("Login fails and error toast appears with invalid credentials", async () => {
+  // GIVEN:
+  await driver.get("http://localhost:3000/login");
+  const invalidEmail = "invalid-email@test.com";
+  const invalidPassword = "InvalidPassword123";
 
-    // THEN:
-    // Attempt login with incorrect credentials
-    await login(driver, invalidEmail, invalidPassword);
-
-    // Check for an error message on the login screen
-    await expectFailMsg(driver, "Incorrect password or email");
+  // THEN:
+  // Attempt login with incorrect credentials
+  await login(driver, invalidEmail, invalidPassword);
+  await driver.sleep(500);
+  await driver.wait(
+    until.elementLocated(
+      By.xpath(
+        '//div[@class="Toastify__toast-body"]/div[contains(text(), "Incorrect password or email")]'
+      )
+    ),
+    1000
+  );
+  await driver.sleep(500);
+  // Now fetch the element and its text
+  const alertElement = await driver.findElement(
+    By.xpath(
+      '//div[@class="Toastify__toast-body"]/div[contains(text(), "Incorrect password or email")]'
+    )
+  );
+  const alertText = await alertElement.getText();
+  expect(alertText).toBe("Incorrect password or email");
 });
 
-test('Login fails and error toast appears with empty fields', async () => {
-    await driver.get('http://localhost:3000/login');
+test("Login fails and error toast appears with empty fields", async () => {
+  await driver.get("http://localhost:3000/login");
 
-    // Leave both fields empty and attempt to login
-    const submitButton = await driver.findElement(By.css('button[type="submit"]'));
-    await submitButton.click();
-
-    // Check for an error message on the login screen
-    await expectFailMsg(driver, "Please enter both email and password.");
+  const submitButton = await driver.findElement(
+    By.css('button[type="submit"]')
+  );
+  await submitButton.click();
+  await driver.wait(
+    until.elementLocated(
+      By.xpath(
+        '//div[@class="Toastify__toast-body"]/div[contains(text(), "Please enter both email and password.")]'
+      )
+    ),
+    3000
+  );
+  await driver.sleep(500);
+  // Now fetch the element and its text
+  const alertElement = await driver.findElement(
+    By.xpath(
+      '//div[@class="Toastify__toast-body"]/div[contains(text(), "Please enter both email and password.")]'
+    )
+  );
+  const alertText = await alertElement.getText();
+  expect(alertText).toBe("Please enter both email and password.");
 });
--- client/src/tests/e2e/utils.js
+++ client/src/tests/e2e/utils.js
@@ -1,25 +1,28 @@
 const { By, until } = require("selenium-webdriver");
 
 async function login(driver, email, password) {
-  try{
-    const emailInput = await driver.findElement(By.name("email"));
-    await emailInput.sendKeys(email);
+  const emailInput = await driver.findElement(By.name("email"));
+  await emailInput.sendKeys(email);
 
-    // Find the password input field and enter a demo password
-    const passwordInput = await driver.findElement(By.name("password"));
-    await passwordInput.sendKeys(password);
+  // Find the password input field and enter a demo password
+  const passwordInput = await driver.findElement(By.name("password"));
+  await passwordInput.sendKeys(password);
 
-    // Find the submit button and click it
-    const submitButton = await driver.findElement(
-      By.css('button[type="submit"]')
-    );
-    await submitButton.click();
-  } catch (err) {
-    console.error("Error during login:", err);
-  }
+  // Find the submit button and click it
+  const submitButton = await driver.findElement(
+    By.css('button[type="submit"]')
+  );
+  await submitButton.click();
 }
 
-async function signupNewUser(driver, firstname, lastname, username, email, password) {
+async function signupNewUser(
+  driver,
+  firstname,
+  lastname,
+  username,
+  email,
+  password
+) {
   try {
     const teacherButtonChoice = await driver.findElement(
       By.xpath('//button[contains(., "I\'m a teacher")]')
@@ -30,7 +33,6 @@ async function signupNewUser(driver, firstname, lastname, username, email, passw
     const firstNameInput = await driver.findElement(By.name("firstname"));
     await firstNameInput.sendKeys(firstname);
 
-    // enter last name
     const lastNameInput = await driver.findElement(By.name("lastname"));
     await lastNameInput.sendKeys(lastname);
 
@@ -60,77 +62,93 @@ async function signupNewUser(driver, firstname, lastname, username, email, passw
 
 async function logout(driver) {
   try {
+    await driver.sleep(500);
     const logoutButton = await driver.findElement(
       By.xpath("//button[contains(text(), 'Logout')]")
     );
+    await driver.wait(until.elementIsVisible(logoutButton), 5000);
     await logoutButton.click();
     await driver.wait(until.urlContains("/login"), 1000);
   } catch (err) {
-    console.error("Error during logout:", err);  
+    console.error("Error during logout:", err);
   }
 }
 
 async function deleteTeacherUser(driver) {
-  try {
-    // TODO: navigate to student profile (if deleting student)
+  const settingsElement = await driver.findElement(
+    By.xpath("//span[text()='Settings']")
+  );
 
-    // navigate to settings tab (if deleting teacher)
-    const settingsElement = await driver.findElement(By.xpath("//span[text()='Settings']"));
+  await settingsElement.click();
+  await driver.wait(
+    until.elementLocated(By.xpath("//button[h3[contains(text(), 'Delete')]]")),
+    500
+  );
+  const deleteAccountButton = await driver.findElement(
+    By.xpath("//button[h3[contains(text(), 'Delete')]]")
+  );
+  deleteAccountButton.click();
 
-    // Click the element
-    await settingsElement.click();
-    await driver.wait(until.elementLocated(By.xpath("//button[h3[contains(text(), 'Delete')]]")), 500);
-    const deleteAccountButton = await driver.findElement(By.xpath("//button[h3[contains(text(), 'Delete')]]"));
-    deleteAccountButton.click()
+  const userFullNameElement = await driver.findElement(By.id("user-fullname"));
+  const userFullNameText = await userFullNameElement.getText();
 
-    // get user's first and last name
-    const userFullNameElement = await driver.findElement(By.id('user-fullname'));
-    const userFullNameText = await userFullNameElement.getText();
-    // enter that in input
-    
-    const inputElement = await driver.findElement(By.id('name-input'));
-    await inputElement.sendKeys(userFullNameText)
+  const inputElement = await driver.findElement(By.id("name-input"));
+  await inputElement.sendKeys(userFullNameText);
 
-    // delete user
+  const confirmDeleteButton = await driver.findElement(
+    By.xpath("//button[contains(text(), 'Delete')]")
+  );
+  confirmDeleteButton.click();
+  await driver.wait(until.urlContains("/signup"), 500);
 
-    const confirmDeleteButton = await driver.findElement(
-      By.xpath("//button[contains(text(), 'Delete')]")
-    );
-    confirmDeleteButton.click()
-    await driver.wait(until.urlContains("/signup"), 500);
-
-    // Assert that local storage is empty
-    const userData = await driver.executeScript("return localStorage.getItem('userData');");
-    expect(userData).toBeNull();
-  } catch (err) {
-    console.error("Error during delete user:", err);
-  }
+  // Assert that local storage is empty
+  const userData = await driver.executeScript(
+    "return localStorage.getItem('userData');"
+  );
+  expect(userData).toBeNull();
 }
 
-async function expectFailMsg(driver, expectedErrorMsg) {
-  try {
-    await driver.wait(until.elementLocated(By.xpath(`//div[@class="Toastify__toast-body"]/div[contains(text(), "${expectedErrorMsg}")]`)), 1000);
+async function expectLoginFail(driver, email, password) {
+  await login(driver, email, password);
+  await driver.wait(
+    until.elementLocated(
+      By.xpath(
+        '//div[@class="Toastify__toast-body"]/div[contains(text(), "Incorrect password or email")]'
+      )
+    ),
+    1000
+  );
 
-    // Now fetch the element and its text
-    const alertElement = await driver.findElement(By.xpath(`//div[@class="Toastify__toast-body"]/div[contains(text(), "${expectedErrorMsg}")]`));
-    const alertText = await alertElement.getText();
-    expect(alertText).toBe(expectedErrorMsg);
+  const alertElement = await driver.findElement(
+    By.xpath(
+      '//div[@class="Toastify__toast-body"]/div[contains(text(), "Incorrect password or email")]'
+    )
+  );
+  const alertText = await alertElement.getText();
+  expect(alertText).toBe("Incorrect password or email");
 
-    // Assert that local storage is empty
-    const checkLSData = await driver.executeScript("return localStorage.getItem('userData');");
-    expect(checkLSData).toBeNull();
-  } catch (err) {
-    console.error("Error during login failure expectation:", err);
-  }
+  // Assert that local storage is empty
+  const checkLSData = await driver.executeScript(
+    "return localStorage.getItem('userData');"
+  );
+  expect(checkLSData).toBeNull();
 }
 
 function getRandomString(length) {
-  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  let result = '';
+  const chars =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let result = "";
   for (let i = 0; i < length; i++) {
-      result += chars.charAt(Math.floor(Math.random() * chars.length));
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
   }
   return result;
 }
 
-module.exports = { login, signupNewUser, logout, deleteTeacherUser, expectFailMsg, getRandomString };
+module.exports = {
+  login,
+  signupNewUser,
+  logout,
+  deleteTeacherUser,
+  expectLoginFail,
+  getRandomString,
+};

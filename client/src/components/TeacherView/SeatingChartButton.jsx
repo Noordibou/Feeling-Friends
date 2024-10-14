3d70919f0c4264e2eef9d8911bfea727f033209f
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2024-10-14 09:34:07 -0500

    changed heading tag to p tag to help with accessibility

--- client/src/components/TeacherView/SeatingChartButton.jsx
+++ client/src/components/TeacherView/SeatingChartButton.jsx
@@ -7,24 +7,28 @@ const SeatingChartButton = ({
   isSelected,
   btnImageWhenOpen,
   flexType,
-  buttonSize
+  buttonSize,
 }) => {
   return (
     <>
       <div className={`${buttonSize !== "small" ? "mx-2" : ""}`}>
         <button
-          className={`text-[Poppins] rounded-xl flex items-center ${buttonSize === "small" ? "border-[2px] border-sandwich flex-col-reverse gap-4 py-2 pt-4 w-[130px] text-[13px] justify-center" : "text-[16px] border-[4px] border-sandwich h-20 w-[270px] justify-around" }   ${
+          className={`text-[Poppins] rounded-xl flex items-center ${
+            buttonSize === "small"
+              ? "border-[2px] border-sandwich flex-col-reverse gap-4 py-2 pt-4 w-[130px] text-[13px] justify-center"
+              : "text-[16px] border-[4px] border-sandwich h-20 w-[270px] justify-around"
+          }   ${
             isSelected ? "underline font-[700] bg-sandwich" : "bg-notebookPaper"
           }`}
           onClick={handleClick}
         >
-          <h4 className="md:pt-2">{buttonText}</h4>
+          <p className="md:pt-2">{buttonText}</p>
           <div className="h-6 flex items-center">
-          <img
-          className="w-12"
-            src={!isSelected ? defaultBtnImage : btnImageWhenOpen}
-            alt={buttonText}
-          />
+            <img
+              className="w-12"
+              src={!isSelected ? defaultBtnImage : btnImageWhenOpen}
+              alt=""
+            />
           </div>
         </button>
       </div>

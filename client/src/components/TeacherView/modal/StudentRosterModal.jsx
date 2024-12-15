77a16f0dc4207057902268975a75036069df6884
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2024-12-14 20:01:43 -0600

    styling for mobile is now available for student roster modal

--- client/src/components/TeacherView/modal/StudentRosterModal.jsx
+++ client/src/components/TeacherView/modal/StudentRosterModal.jsx
@@ -46,14 +46,14 @@ const AddStudentModal = forwardRef(
     return createPortal(
       <dialog
         ref={studentModalRef}
-        className="rounded overflow-hidden bg-notebookPaper border-sandwich border-4"
+        className="rounded overflow-visible bg-notebookPaper border-sandwich border-4"
       >
         <div className="w-full md:w-auto flex justify-center items-center">
-          <div className="max-h-[90vh] w-[85%] md:w-[686px] px-10 sm:p-10 rounded">
+          <div className="max-h-[90vh] w-[85%] md:w-[686px] px-2 sm:p-10 rounded">
             <form method="dialog" onSubmit={onClose}>
               <button>
                 <img
-                  className="absolute -top-6 -right-6 "
+                  className="absolute -top-4 -right-4 "
                   src={CancelImg}
                   alt="close student roster"
                   tabIndex="0"
@@ -66,7 +66,7 @@ const AddStudentModal = forwardRef(
                 <h2 className="font-[Poppins] text-[20px] md:text-[24px] my-5">
                   Tap to add students to the classroom
                 </h2>
-                <div className="flex flex-row h-[60vh] md:h-3/4 flex-wrap overflow-y-auto">
+                <div className="flex flex-row h-[60vh] md:h-3/4 max-h-[300px] flex-wrap overflow-y-auto">
                   <UnassignedStudent
                     unassignedStudents={unassignedStudents}
                     students={students}
@@ -75,7 +75,7 @@ const AddStudentModal = forwardRef(
                   />
                 </div>
 
-                <div className="flex items-end justify-center mt-4">
+                <div className="flex items-end justify-center sm:mt-4 my-10">
                   <button
                     id="unassigned-section"
                     className="flex items-center h-[50px] md:h-[90px] w-full flex-col rounded-2xl border-4 border-darkSandwich"

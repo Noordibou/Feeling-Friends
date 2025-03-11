d4aed75cef6e93b3b48173040fdef4ab80f237cb
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2025-03-10 21:26:46 -0500

    cleaned up text for non-edit page

--- client/src/components/TGreeting.jsx
+++ client/src/components/TGreeting.jsx
@@ -11,9 +11,7 @@ const Greeting = ({ isEditMode, userData }) => {
         <header className="hidden md:flex items-start justify-end underline mt-4 lg:mt-0 lg:mb-12 mr-4 ">
           <Logout location="teacherLogout" userData={userData} />
         </header>
-        <h1 className="font-[Poppins] text-[32px] font-bold sm:mx-6">
-          Edit Classes
-        </h1>
+        <h1 className="font-[Poppins] text-[32px] sm:mx-6">Edit Classes</h1>
         <h4 className="font-[Poppins] sm:text-[20px] pb-[0.5rem] sm:mx-6">
           Add or remove classes, set a class as the dashboard default, or tap to
           manage student rosters.
@@ -37,19 +35,14 @@ const Greeting = ({ isEditMode, userData }) => {
   }
 
   return (
-    <section className="font-body text-start lg:mt-16 mt-20 md:mt-0">
+    <section className="font-body text-start lg:mt-16 mt-10 md:mt-0">
       <div className="hidden md:flex items-start justify-end underline lg:-mt-16 lg:mb-20 mr-4 md:pt-8 ">
         <Logout location="teacherLogout" userData={userData} />
       </div>
-      <div className="ml-10 mt-4">
-        <h1 className="text-header4 md:text-header1 font-header1 text-start">
+      <div className="sm:ml-10 mt-4">
+        <h1 className="text-[32px] font-bold">
           {greeting}, {userData.prefix} {userData.firstName}!
         </h1>
-        {userData && (
-          <h2 className="text-button md:text-header4 font-header3 text-start pb-2">
-            Your Classes at a Glance
-          </h2>
-        )}
       </div>
     </section>
   );
